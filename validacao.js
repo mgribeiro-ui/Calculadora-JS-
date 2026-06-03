3d3c6b07cd7cd807327de3dbea368a42109b50e3
MARCUS VINICIUS <mg.ribeiro@uni9.edu.br>
2026-06-03 11:34:11 +0000

    marcaçoes

--- validacao.js
+++ validacao.js
@@ -1,8 +1,8 @@
-// Passo 3: Selecione o formulário com document.getElementById()
+
 const formulario = document.getElementById("meuFormulario");
 const paragrafoErro = document.getElementById("erro");
 
-// Passo 4: Adicione um addEventListener("submit", ...) no formulário
+
 formulario.addEventListener("submit", function(event) {
     
     // Capturando os valores dos campos
@@ -10,10 +10,10 @@ formulario.addEventListener("submit", function(event) {
     const email = document.getElementById("email").value;
     const idade = parseInt(document.getElementById("idade").value);
     
-    // Variável para armazenar as mensagens de erro
+   
     let mensagemErro = "";
 
-    // Passo 5: Dentro do evento, valide os campos
+   
     if (nome === "") {
         mensagemErro += "O nome não pode estar vazio. ";
     }
